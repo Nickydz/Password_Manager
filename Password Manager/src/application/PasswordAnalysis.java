292b72a3d6d87d2ffc1f428c19933f09b3b46857
DevenBhalerao <devenbhalerao@gmail.com>
2016-03-02 00:05:24 +0530

    Fxml cotroller connection established

--- Password Manager/src/application/PasswordAnalysis.java
+++ Password Manager/src/application/PasswordAnalysis.java
@@ -1,6 +1,48 @@
 package application;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
 public class PasswordAnalysis {
 
+	@FXML 
+	private TextField PasswordInput;
+	
+	@FXML 
+	private ProgressBar Progress;
+	
+	@FXML 
+	private Label PWComment;
+	
+	@FXML 
+	private Label PWLength;
+	
+	@FXML 
+	private Label NumberofChars;
+	
+	@FXML 
+	private Label NumberofNums;
+	
+	@FXML 
+	private Label NumofUpperC;
+	
+	@FXML 
+	private Label NumofSymbols;
+	
+	@FXML 
+	private Label NumofLowerC;
+	
+	@FXML 
+	private Label NumofRepeatedChar;
+	
+	@FXML 
+	private Text PWQualityMetrics;
+	
+	
+	
+	
 	
 }
