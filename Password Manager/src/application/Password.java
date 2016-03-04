763bd47893ee5d06965f91fbde14c75121303dc9
DevenBhalerao <devenbhalerao@gmail.com>
2016-03-05 00:09:31 +0530

    Code for key type event

--- Password Manager/src/application/Password.java
+++ Password Manager/src/application/Password.java
@@ -8,17 +8,17 @@ public class Password {
     private StringProperty password = new SimpleStringProperty(this, "password", "");
 
     //Returns the StringProperty object
-    public StringProperty firstNameProperty() {
+    public StringProperty PasswordProperty() {
         return password;
     }
 
     //Return the firstName value (ie. "Bucky")
-    public String getFirstName() {
+    public String getPassword() {
         return password.get();
     }
 
     //Set the firstName value
-    public void setFirstName(String password) {
+    public void setPassword(String password) {
         this.password.set(password);
     }
 
--- Password Manager/src/application/PasswordAnalysis.java
+++ Password Manager/src/application/PasswordAnalysis.java
@@ -3,58 +3,60 @@ package application;
 import javafx.beans.property.StringProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.text.Text;
 
 public class PasswordAnalysis {
-	
-	
+
+
 	Password password = new Password();
 
 	@FXML 
 	private TextField PasswordInput;
-	
+
 	@FXML 
 	private ProgressBar Progress;
-	
+
 	@FXML 
 	private Label PWComment;
-	
+
 	@FXML 
 	private Label PWLength;
-	
+
 	@FXML 
 	private Label NumberofChars;
-	
+
 	@FXML 
 	private Label NumberofNums;
-	
+
 	@FXML 
 	private Label NumofUpperC;
-	
+
 	@FXML 
 	private Label NumofSymbols;
-	
+
 	@FXML 
 	private Label NumofLowerC;
-	
+
 	@FXML 
 	private Label NumofRepeatedChar;
-	
+
 	@FXML 
 	private Text PWQualityMetrics;
 	
+	@FXML 
+	private Button button;
+
 	@FXML
-	private void onChange(ActionEvent event){
-		 password.firstNameProperty().addListener((v,oldValue,newValue) -> 
-		 
-				 );
-	
-		
+	private void onChange(KeyEvent event){
+		System.out.println("lol");
+
 	}
-	
-	
-	
+
+
+
 }
