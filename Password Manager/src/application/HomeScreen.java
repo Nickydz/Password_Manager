2dd66340165666bd16258a96af3f94c1be15d229
DevenBhalerao <devenbhalerao@gmail.com>
2016-03-12 23:48:26 +0530

    Added buttons for home screen and created fxmls

---
+++ Password Manager/src/application/HomeScreen.java
@@ -0,0 +1,34 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class HomeScreen {
+
+	@FXML
+	Button addEntryBT;
+	
+	@FXML
+	Button deleteEntryBT;
+	
+	@FXML
+	Button PWGenerationBT;
+	
+	@FXML
+	Button PWAnalysisBT;
+	
+	@FXML
+	Button SNotesBT;
+	
+	@FXML
+	Button AccSettingsBT;
+	
+	@FXML
+	Button SearchBTBT;
+	
+	@FXML
+	Button LogOutBT;
+	
+	
+	
+}
--- Password Manager/src/application/Home_Dialog_Box.java
+++ Password Manager/src/application/Home_Dialog_Box.java
@@ -1,5 +1,36 @@
 package application;
 
-public class Home_Dialog_Box {
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
+public class Home_Dialog_Box {
+	
+	
+
+	public void display(String function){
+		if(function.equals("add"))
+			addDialogBox();
+		if(function.equals("delete"))
+			deleteDialogBox();
+		
+	}
+
+	private void deleteDialogBox() {
+		// TODO Auto-generated method stub
+		
+		
+		
+	}
+
+	private void addDialogBox() {
+		// TODO Auto-generated method stub
+		
+		
+		
+		
+		
+	}
+	
 }
